b0002f675909d44fe1a738f3104843235f46e0fc
Atharva Malode <Atharvamalode21@gmail.com>
2022-07-05 20:05:05 +0530

    backtrack

---
+++ backtraking/array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void print(int array[], int n ){
+       for (int j = 0; j < n ; j++){
+        cout << array[j]<<" ";
+       }
+}
+void Backtrack(int *arr, int n,int i, int value)
+{ 
+
+  //base case
+  if(i == n){
+     print(arr,n);
+      return;
+     }
+  
+  //recursive case
+    arr[i] = value;
+     Backtrack(arr,n, i + 1 ,value + 1);
+  // backtrack case
+  arr[i] = -1 * arr[i];
+}
+
+int main()
+{
+  int arr[100] ;
+  //int size = sizeof(arr) / sizeof(int);
+  int n;
+  cin>>n;
+
+  Backtrack(arr,n, 0, 1);
+  cout <<endl;
+  print(arr,n);
+}
