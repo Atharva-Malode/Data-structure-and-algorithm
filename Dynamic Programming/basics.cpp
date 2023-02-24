7111286c073497155fc770e3cd99f82baf3ca58d
Atharva Malode <Atharvamalode21@gmail.com>
2023-02-24 23:40:18 +0530

    updated stl

--- Dynamic Programming/basics.cpp
+++ Dynamic Programming/basics.cpp
@@ -4,10 +4,12 @@ using namespace std;
 // It is a Top down Appraoch
 int top(vector<int> &dp, int n)
 {
+    // base case
     if (dp[n] != -1)
     {
         return dp[n];
     }
+    // check weathere the step is already computed or not
     if (n <= 1)
     {
         dp[n] = n;
@@ -20,6 +22,16 @@ int top(vector<int> &dp, int n)
 // Bottom down approach
 int bottom(vector<int> &Dp, int n)
 {
+    // base case
+    Dp[0] = 0;
+    Dp[1] = 1;
+
+    // bottom approach using loop
+    for (int i = 2; i <= n; i++)
+    {
+        Dp[i] = Dp[i - 1] + Dp[i - 2];
+    }
+    return Dp[n];
 }
 int main()
 {
@@ -28,5 +40,8 @@ int main()
     cin >> no;
     vector<int> dp(no + 1, -1);
     cout << "The " << no << "th Fibonacci number is: " << top(dp, no) << endl;
+    vector<int> Dp(no + 1, -1);
+    cout << "The " << no << "th Fibonacci number is: " << bottom(Dp, no) << endl;
+
     return 0;
 }
---
+++ Dynamic Programming/coin_change.cpp
@@ -0,0 +1,27 @@
+/*
+Given array of coin denominations, and integer m
+representing target Money. We need to find the min coin
+required to make the changes.
+
+
+
+Input   : coins[1,3,7,10], m = 15
+  Explanation : here there is a array of coins and m is
+  the target value you have to give minimum no coins required to
+  complete the target
+
+
+Output  : 3 (7+7+1)
+  Explanation : In the above output minimum 3 coins 7 + 7 + 1 hits the target
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> coins = {1, 3, 7, 10};
+  int m = 15;
+
+  return 0;
+}
