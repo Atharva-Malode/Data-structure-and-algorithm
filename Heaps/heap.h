86961f49ba78748e0cd87b480019f1fa8993cacc
Atharva Malode <Atharvamalode21@gmail.com>
2023-05-02 00:31:25 +0530

    operations in heap

--- Heaps/heap.h
+++ Heaps/heap.h
@@ -5,7 +5,26 @@ using namespace std;
 class Heap 
 {
     vector <int> v;
+    void Heapify(int i)
+    {
+      int mididx   = i;
+      int left  = 2 * i;
+      int right = 2*i +1;
 
+      if(left < v.size() and v[left] < v[i])
+      {
+           int midIdx = left;
+      }
+      if (right < v.size() and v[right] < v[mididx])
+      {
+          mididx = right;
+      }
+      if(mididx != i)
+      {
+        swap(v[i],v[mididx]);
+        Heapify(mididx);
+      }
+    }
   public:
      Heap (int default_size = 10)
      {
@@ -16,7 +35,7 @@ class Heap
      void push(int data)
      {
           v.push_back(data);
-          int idx = v.size()+1;
+          int idx = v.size()-1;
           int parent = idx/2;
 
           while(idx > 1 and v[idx] < v[parent])
@@ -26,5 +45,21 @@ class Heap
                parent = parent/2;
           } 
      }
-    
+     int getMin(int data)
+     {
+        return v[1];
+     }
+     void pop()
+     {
+        //swap the top and bottom most element and call heapify
+        int idx = v.size()-1;
+        swap (v[1],v[idx]);
+        v.pop_back();
+        Heapify(1);
+     }
+
+     bool empty()
+     {
+        return v.size() == 1;
+     }
 };
