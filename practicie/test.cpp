db67c4e455ace1fe51994381d32b6992971347d8
Atharva Malode <Atharvamalode21@gmail.com>
2023-05-07 22:35:46 +0530

    few data structure's restructuring

--- practicie/test.cpp
+++ practicie/test.cpp
@@ -1,47 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-class node
-{
-public:
-    int data;
-    node *next;
-
-    node(int d) : data(d), next(NULL) {}
-};
-
-class List
-{
-    // public:
-    node *head;
-    node *tail;
-
-public:
-    List() : head(NULL), tail(NULL) {}
-
-    void push_front(int data)
-    {
-        if (head == NULL)
-        {
-            node *n = new node(data);
-            head = tail = n;
-        }
-        else
-        {
-            node *n = new node(data);
-            n->next = head;
-            head = n;
-        }
-    }
-    void push_back(int data)
-    {
-    }
-};
 
 int main()
 {
 
-    List a;
-    a.push_front(10);
+     string a = "Hello";
+
+     for (int i = 0 ; i < a.length(); i++)
+     {
+        cout << a[i]<<" ";
+     }
+
+     
+
     return 0;
 }
