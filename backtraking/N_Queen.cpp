3668790e37e9b6e1b82939ac2446be5f1ef95cdb
Atharva Malode <Atharvamalode21@gmail.com>
2023-05-14 15:54:01 +0530

    Problem : N queen

--- backtraking/N_Queen.cpp
+++ backtraking/N_Queen.cpp
@@ -2,33 +2,59 @@
      The N queen problem is only for the 1 possible solution 
      All ways N - queen problem is in the next cpp file
 */
-
-
 #include <iostream>
-
 using namespace std;
 
 //to check weather the Queen can be placed here or not
-bool CanPlace(int board[][20],int n, int i, int j)
+bool CanPlace(int board[][20],int n, int x, int y)
 {
     //there will be only one queen in the row as per our code no need to check for that 
 
-
     //for column i
-    for (int k = 0; k < i; k++)
+    for (int k = 0; k < x; k++)
     {
-          
+          if(board[k][y] == 1)
+          {
+            return false;
+          }
     }
 
+    // for diagonal on left
+    int i = x;
+    int j = y;
+
+    while (i >= 0 and j >= 0)
+    {
+      if (board[i][j] == 1)
+      {
+        return false;
+      }
+      i--,j--;
+    }
+
+    //for diagonal on right
+
+     i = x;
+     j = y;
+   while (i >= 0 and j < n)
+   {
+     if(board[i][j] == 1)
+     {
+      return false;
+     }
+    i--, j--;
+   }
+
+   return true;
 
 }
 
 // the given function will print the matric with the queen as one and rest as 0
 void printBoard(int board [][20], int n)
 {
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-      for (int j = 0; j <= n; j++)
+      for (int j = 0; j < n; j++)
       {
           cout << board[i][j]<<" ";
       }
@@ -45,32 +71,33 @@ bool SolveNqueen(int board[][20],int n , int i)
     printBoard(board,n);
      return true;
  }
- //recursive case
  
+ 
+ //recursive case
  for (int j=0; j<n; j++)
  {
       if(CanPlace(board,n,i,j))
       {
-        board[i][j] == 1;
+        board[i][j] = 1;
         bool Succese = SolveNqueen(board,n,i+1);
         if(Succese)
         {
           return true;
         }
         //Backtrack
-        board[i][j] == 0;
+        board[i][j] = 0;
       }
 
-      return false;
  }
+      return false;
 
 }
 int main ()
 {
 
    int n;
-   cin >> n;
    int board [20][20] = {0};
+   cin >> n;
    SolveNqueen(board,n, 0);
 
   return 0;
